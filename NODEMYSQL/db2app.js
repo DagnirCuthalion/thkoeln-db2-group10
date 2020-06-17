1fd6efc9bd29c7263bc92bc5a7d2ef090a55f56d
lr <lukas.radigk@smail.th-koeln.de>
2020-06-17 19:53:37 +0200

    dynamic stuff add

--- NODEMYSQL/db2app.js
+++ NODEMYSQL/db2app.js
@@ -44,8 +44,8 @@ app.listen('3000', () =>{
     console.log('Server started on port 3000');
 });
 
-
 //Get Person
+
 app.get('/person/add',(req,res)=>{
 
     res.render('pages/add-event.ejs',{
@@ -53,7 +53,7 @@ app.get('/person/add',(req,res)=>{
         pageTitle: "Add Person",
         items : '' 
     });
-});
+}); 
 
 
 
@@ -105,34 +105,25 @@ app.get('/person',(req,res)=>{
 
 //Show Labore
 app.get('/labor',(req,res)=>{
-
+    //console.log('getting labor');
     db.query("SELECT * FROM labor", (err,result)=>{
-        res.render('pages/index',{
+        res.render('pages/get-dynamic.ejs',{
             siteTitle : siteTitle,
             pageTitle: "Labor List",
             items : result 
         });
+        //console.log(result);
     })
-});
-
-//Add Labor
-app.get('/labor/add',(req,res)=>{
-
-    res.render('pages/add-event.ejs',{
-        siteTitle : siteTitle,
-        pageTitle: "Add Labor",
-        items : '' 
-    });
 });
 
 //Insert sample data
 //Post Labor
 app.post('/labor/add',(req,res)=>{
-
+    //console.log('posting laboradd');
     var query =     "INSERT INTO `labor` (labor_id,labor_name) VALUES ("
         query +=    " '"+req.body.labor_id+"',";
         query +=    " '"+req.body.labor_name+"')";
-    
+    //console.log('post laboradd query: ' + query);
     db.query(query,(err,result)=>{
         res.redirect("/labor");
     });
@@ -145,5 +136,98 @@ app.get('/updatelabor/:id',(req,res)=>{
         if(err)throw err;
         console.log(result);
         res.send('Labor updated..');
+    });
+});
+
+
+
+app.get('/test',(req,res)=>{
+    res.render('pages/test',{
+        siteTitle : siteTitle,
+        pageTitle: "View Dynamic"
+    });
+});
+
+
+class DynamicInput {
+
+    constructor(arr, tablename, action)
+    {
+        this.arr = arr;
+        this.tablename = tablename;
+        this.action = action;
+    }
+}
+
+app.post('/dynamic',(req,res)=>{
+    var dynIn = new DynamicInput(Array(0),'tablename_u_shall_not_see_this','action_u_shall_not_see_this');
+    console.log('postdynamic tablename: '+req.body.tablename);
+    db.query("SELECT * FROM "+req.body.tablename,(err,result)=>{
+        //console.log('result: '+result);
+        //console.log('err: '+err);
+        var resultArray = Object.values(JSON.parse(JSON.stringify(result)));
+        //console.log('resArr: ' + resultArray);
+        var numberOfAttributes;
+        if(typeof(resultArray[1])=='undefined')
+            numberOfAttributes = 0;
+        else
+            numberOfAttributes=resultArray[1].size
+        dynIn = new DynamicInput(Array(numberOfAttributes),req.body.tablename, req.body.action);
+        resultArray.forEach(function(v)
+        {
+            //console.log('v: '+v);
+            i=0;
+            for (var key in v) {
+                //console.log('v.key: '+key);
+                if (Object.prototype.hasOwnProperty.call(v, key)) {
+                    var val = v[key];
+                    // use val
+                    //console.log('v.val: '+val);
+                    dynIn.arr[i++] = {name:key, value:val}
+                }
+            }
+        })
+        //dynIn.arr.forEach(function(e){console.log(e)});
+        if(dynIn.action=='action_u_shall_not_see_this'||typeof(dynIn.action)=='undefined')
+        {
+            console.log('>>dynIn.action was not defined!!<<')
+            dynIn.action="http://localhost:3000/"+req.body.tablename+'/add';
+        }
+            
+        //console.log('dynIn.arr:')
+        //dynIn.arr.forEach(function(e){console.log('e: '+e)});
+        //console.log('action: ' + dynIn.action);
+
+        console.log('should be rendering form now');
+        res.render('pages/add-form-dynamic.ejs',{
+        siteTitle : siteTitle,
+        pageTitle: "hardcoded_Page Title",
+        dynIn
+        });
+    });
+});
+
+app.get('/', (req,res)=>{
+    res.render('pages/main',
+        {
+            siteTitle : siteTitle,
+            pageTitle: "hardcoded_Main"
+        }
+    )
+})
+
+app.post('/dynamic/add',(req,res)=>{
+
+    var query =     "INSERT INTO `<% req.body.tablename %>` (";
+        console.log(req.body);
+        //query -= ','
+            
+        query += ") VALUES (";
+        
+        //query -= ',';
+        query += ")";
+    
+    db.query(query,(err,result)=>{
+        res.redirect("/");
     });
 });
